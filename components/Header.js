ff271437bcea750975a7d50603df88f19f253a98
Szilárd Salamon <salamon.szilard@gbsolutions.io>
2022-10-27 10:29:25 +0200

    data fetching

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return(
+      <div>
+          <h1 className="title">ez itten most a header</h1>
+          <style jsx>
+              {`
+                  .title{
+                  color:red;
+                  
+              }
+              `}
+          </style>
+      </div>
+  )
+}
+
+export  default  Header
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-export default function Home() {
-  return (
+export default function Home({ comments }) {
+    return (
     <div>
         <Head>
             <title>Test App</title>
             <meta name="description" content="ez egy test"/>
         </Head>
         <h1>Szia Andras</h1>
+        <p>Kiprobalom a data fetchinget</p>
+
+        {comments.map((comment) => (
+            <h3>{comment.name}</h3>
+        ))}
     </div>
   )
+}
+
+export const  getStaticProps = async () => {
+    const  res = await fetch("https://jsonplaceholder.typicode.com/post/1/comments")
+    const  comments = await res.json()
+
+    return{
+        props: {
+            comments
+        }
+    }
 }
